6dd02665c9a6f54160d2a7f30b57d1d0f6c915d8
MichaelChen <s940503g@hotmail.com>
2018-05-30 01:59:31 +0000

    Add default model.

--- models.py
+++ models.py
@@ -1,32 +1,39 @@
 from keras.datasets import cifar10
-from keras.layers import Input, Dense, GlobalAveragePooling2D, Activation
-from keras.applications import vgg16
+from keras.layers import Input, Dense, GlobalAveragePooling2D, Conv2D, MaxPooling2D, Flatten
 from keras.models import Model
 from keras.utils import to_categorical
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-x_train = vgg16.preprocess_input(x_train.astype('float64'))
-x_test = vgg16.preprocess_input(x_test.astype('float64'))
+def default_model():
+	input = Input(shape=(32,32,3), name='custom_inputs')
+	net = Conv2D(64, (3,3))(input)
+	net = Conv2D(64, (1, 1))(net)
+	net = MaxPooling2D((2,2))(net)
+	net = Conv2D(128, (3,3))(net)
+	net = Conv2D(128, (1, 1))(net)
+	net = MaxPooling2D((2,2))(net)
 
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
+	net = Flatten()(net)
+	net = Dense(512)(net)
+	net = Dense(512)(net)
+	predictions = Dense(10, activation='softmax')(net)
 
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+	model = Model(inputs=input, outputs=predictions)
+	model.summary()
 
-base_model = vgg16.VGG16(include_top=False, weights='imagenet')
+	return model
+if __name__ == "__main__":
+	model = default_model()
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-input = Input(shape=(32,32,3), name='custom_inputs')
-net = base_model(input)
-net = GlobalAveragePooling2D()(net)
-predictions = Dense(10, activation='softmax')(net)
+	(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-model = Model(inputs=input, outputs=predictions)
+	y_train = to_categorical(y_train, 10)
+	y_test = to_categorical(y_test, 10)
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+	from keras.preprocessing.image import ImageDataGenerator
+	train_datagen = ImageDataGenerator(vertical_flip=True, horizontal_flip=True, rotation_range=180, rescale=1/255.)
+	test_datagen = ImageDataGenerator(rescale=1/255.)
 
-model.summary()
-model.fit(x_train, y_train, validation_data=(x_test, y_test), verbose=1)
+	train_data = train_datagen.flow(x_train, y_train, batch_size=128)
+	val_data = test_datagen.flow(x_test, y_test, batch_size=128) 
+	model.fit_generator(train_data, validation_data=val_data, verbose=1, epochs=50)
